1c7e8e73a12156c3f88c358914bfe4dc20f890bb
aprxrt3 <ajiprxty3@gmail.com>
2021-11-16 13:15:50 +0700

    remove & custom fonts

--- lib/fontstyle.dart
+++ lib/fontstyle.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 TextStyle mainHeader = const TextStyle(
-  fontSize: 26,
-  color: Colors.black38,
-  fontFamily: 'Poppins',
-);
+    fontSize: 26,
+    color: Colors.black38,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700);
 
 TextStyle subHeader = const TextStyle(
   fontSize: 16,
   color: Colors.blueGrey,
   fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
 );
