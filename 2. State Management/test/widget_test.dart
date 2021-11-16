8b7a96868f83d3f9b44620e5b45bc686023e69b0
aprxrt3 <ajiprxty3@gmail.com>
2021-11-16 21:43:43 +0700

    chore

--- 2. State Management/test/widget_test.dart
+++ 2. State Management/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kohi_apps/main.dart';
 
-import 'package:payment_apps/main.dart';
+import 'package:kohi_apps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( kohi_apps());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
