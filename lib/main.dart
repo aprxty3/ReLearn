2dc6661317f57361dfeda988c8aeb45c41a5ff30
aprxrt3 <ajiprxty3@gmail.com>
2021-11-16 13:07:56 +0700

    set axisalignment

--- lib/main.dart
+++ lib/main.dart
@@ -12,56 +12,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
           appBar: AppBar(
             title: const Text("Cashy"),
             backgroundColor: Colors.red,
           ),
           body: SafeArea(
-            child: Container(
-                // color: Colors.indigo,
-                margin: const EdgeInsets.only(
-                  left: 60.0,
-                  top: 0,
-                  right: 0,
-                  bottom: 0,
-                ),
-                padding: const EdgeInsets.only(
-                  left: 0,
-                  top: 50.0,
-                  right: 0,
-                  bottom: 0,
-                ),
-                child: Column(
-                  //penggunaan ROW untuk ke kanan COLUMN ke bawah
-                  children: <Widget>[
-                    const Image(
-                      image: AssetImage('assets/images/data.jpg'),
-                      height: 200,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 20.0,
-                        bottom: 4.0,
+              child: Container(
+                  // color: Colors.indigo,
+                  margin: const EdgeInsets.only(
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                  ),
+                  padding: const EdgeInsets.only(
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        //penggunaan ROW untuk ke kanan COLUMN ke bawah
+                        children: <Widget>[
+                          const Image(
+                            image: AssetImage('assets/images/data.jpg'),
+                            height: 200,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 20.0,
+                              bottom: 4.0,
+                            ),
+                            child: Text(
+                              "Data is Killing",
+                              style: mainHeader,
+                            ),
+                          ),
+                          Text(
+                            "Trust me",
+                            style: subHeader,
+                            textAlign: TextAlign.center,
+                          )
+                        ],
                       ),
-                      child: Text(
-                        "Data is Killing",
-                        style: mainHeader,
-                      ),
-                    ),
-                    Text(
-                      "Trust me",
-                      style: subHeader,
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                )),
-          )),
-    );
+                    ],
+                  ))),
+        ));
   }
 }
