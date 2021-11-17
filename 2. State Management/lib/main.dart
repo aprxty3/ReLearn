81e4eacd70e50a14f80110997d7df3ca12257db1
aprxrt3 <ajiprxty3@gmail.com>
2021-11-17 09:57:12 +0700

    add header

--- 2. State Management/lib/main.dart
+++ 2. State Management/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kohi_apps/theme.dart';
 
 void main() {
   runApp(kohi_apps());
@@ -10,10 +11,41 @@ class kohi_apps extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Color(0xffD1E1E0),
+        // appBar: AppBar(
+        //   backgroundColor: Color(0xffD1E1E0),
+        //   title: Text(
+        //     'Kohi',
+        //     style: UpText,
+        //   ),
+        // ),
         body: Column(
           children: <Widget>[
-            Image.asset(
-              'assets/images/1.png',
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 84.0, right: 99, top: 84),
+                child: Image.asset(
+                  'assets/images/1.png',
+                  width: 192,
+                  height: 243,
+                  alignment: Alignment.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 58, right: 73, bottom: 10),
+              child: Text(
+                'Caramel Macchiato',
+                style: titleText,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 40, right: 55, bottom: 30),
+              child: Text(
+                'We cannot guarantee that any unpackaged \nproducts served in our stores are allergen-free',
+                style: subText,
+                textAlign: TextAlign.center,
+              ),
             ),
           ],
         ),
--- 2. State Management/lib/theme.dart
+++ 2. State Management/lib/theme.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle titleText = GoogleFonts.poppins(
-  color: Color(0xff023E4A),
+  color: const Color(0xff023E4A),
   fontWeight: FontWeight.bold,
   fontSize: 24,
 );
 
 TextStyle subText = GoogleFonts.poppins(
-  color: Color(0xff687A79),
+  color: const Color(0xff687A79),
   fontWeight: FontWeight.w500,
   fontSize: 16,
+  
 );
 
 TextStyle buttonText = GoogleFonts.poppins(
-  color: Color(0xffFFFFFF),
+  color: const Color(0xffFFFFFF),
   fontWeight: FontWeight.w700,
   fontSize: 14,
+);
+
+TextStyle UpText = GoogleFonts.poppins(
+  color: Colors.black87,
+  fontWeight: FontWeight.bold,
+  fontSize: 24,
 );
