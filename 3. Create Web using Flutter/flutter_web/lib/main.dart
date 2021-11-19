d0bd405ed732a60d7fda7ab097a15f2466e4b831
aprxrt3 <ajiprxty3@gmail.com>
2021-11-19 07:27:50 +0700

    bug fix

--- 3. Create Web using Flutter/flutter_web/lib/main.dart
+++ 3. Create Web using Flutter/flutter_web/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -9,9 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Image.asset('assets/illustrasi.png'),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                'Welcam',
+                style: GoogleFonts.poppins(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'Gimana kabsnya Brow?',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
