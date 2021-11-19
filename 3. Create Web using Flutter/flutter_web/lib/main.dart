a8527289b9c5097d6079d2f9bc4a3395207aba07
aprxrt3 <ajiprxty3@gmail.com>
2021-11-19 07:17:13 +0700

    installing google font

--- 3. Create Web using Flutter/flutter_web/lib/main.dart
+++ 3. Create Web using Flutter/flutter_web/lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: Image.asset('assets/illustrasi.png'),
-    ));
+          body: Image.asset('assets/illustrasi.png'),
+        ));
   }
 }
