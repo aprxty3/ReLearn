459d26525da4e9f2a631cde6ae53518d02a998e1
aprxrt3 <ajiprxty3@gmail.com>
2021-11-19 08:19:02 +0700

    slicing Text

--- 3. Create Web using Flutter/flutter_web/lib/page.dart
+++ 3. Create Web using Flutter/flutter_web/lib/page.dart
@@ -7,21 +7,121 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 40,
+          horizontal: 100,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              'Welcam',
-              style: GoogleFonts.poppins(
-                fontSize: 30,
-              ),
+            Image.asset(
+              'logo.png',
+              width: 72,
+              height: 40,
             ),
-            Text(
-              'Gimana kabsnya Brow?',
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            )
+            Row(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Guide',
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xff1D1E3C),
+                      ),
+                    ),
+                    Container(
+                      width: 66,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xffFE998D),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Pricing',
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: const Color(0xff1D1E3C),
+                      ),
+                    ),
+                    Container(
+                      width: 66,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.transparent,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Team',
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: const Color(0xff1D1E3C),
+                      ),
+                    ),
+                    Container(
+                      width: 66,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.transparent,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Stories',
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: const Color(0xff1D1E3C),
+                      ),
+                    ),
+                    Container(
+                      width: 66,
+                      height: 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.transparent,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Image.asset(
+              'button_account.png',
+              width: 163,
+              height: 53,
+            ),
           ],
         ),
       ),
